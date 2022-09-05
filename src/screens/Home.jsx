fccf82939029b700170222b1487933cb85c8e97f
JL-III <jesselopez1162@gmail.com>
2022-09-04 21:34:35 -0500

    Update Home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <>
       <h1 style={{ fontSize: "5vh" }}>Theatria</h1>
+      <h2 style={{ fontSize: "3.5vh" }}>mc.playtheatria.com</h2>
       <div>
         <News></News>
       </div>
