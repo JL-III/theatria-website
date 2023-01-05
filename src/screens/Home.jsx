f0fea1b6019aca84718166cf61a8312fea1d1342
JL-III <jesselopez1162@gmail.com>
2023-01-05 16:17:48 -0600

    add jank discord link

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <>
       <h1 style={{ fontSize: "5vh" }}>Theatria</h1>
       <h2 style={{ fontSize: "3.5vh" }}>mc.playtheatria.com</h2>
+        <a style={{ fontSize: "3.5vh" }} href={"https://discord.gg/xegrV85Au4"}> Discord </a>
       <div>
         <News></News>
       </div>
