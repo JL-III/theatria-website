52df212199f34eb322b65a71f5954bbe6998cb93
JL-III <jesselopez1162@gmail.com>
2023-01-09 19:48:22 -0600

    fix warns

--- src/swiper.jsx
+++ src/swiper.jsx
@@ -6,7 +6,7 @@ import slide4 from "./resources/2022-12-30_18.27.59.png"
 import slide5 from "./resources/2023-01-07_22.34.19.png"
 import './swiper.css'
 
-import SwiperRIGGED, {Autoplay} from 'swiper';
+import {Autoplay} from 'swiper';
 import { Swiper, SwiperSlide} from 'swiper/react';
 
 
@@ -14,7 +14,7 @@ import { Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/bundle';
 
-export default () => {
+const SwiperThing = () => {
     return (
         <Swiper id="swiper-scroll"
             modules={[Autoplay]}
@@ -33,3 +33,5 @@ export default () => {
         </Swiper>
     );
 };
+
+export default SwiperThing;
